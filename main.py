598a72031902e29dc0b8053ceb9c5ba3e262f729
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-05-29 23:31:15 +0330

    modularized the code

--- main.py
+++ main.py
@@ -1,49 +1,32 @@
 # Importing dependecies
 import random
+from population import Population
 
 # Setting parameters
-POPULATION_SIZE = 200
+POPULATION_SIZE = 100
 MAX_GENERATIONS = 1000 
 MUTATION_RATE = 0.2
 
-# Generating intial population
-def init_population(board_size, size):
-    population = [
-        random.sample(range(board_size), board_size) for _ in range(size)
-    ]
-    return population
-
-# Single-point crossover function
-def crossover(parent1, parent2):
-    crossover_point = random.randint(0, len(parent1) - 1)
-    child1 = parent1[:crossover_point] + parent2[crossover_point:]
-    child2 = parent2[:crossover_point] + parent1[crossover_point:]
-    return child1, child2
-
-# Function to perform mutation on a chromosome
-def mutate(chromosome):
-    n = len(chromosome)
-    mutation_point = random.randint(0, n - 1)
-    new_gene = random.randint(0, n - 1)
-    chromosome[mutation_point] = new_gene
-    return chromosome
-
 # Fitness function
-def calculate_fitness(chromosome):
+def calc_fitness(chromosome):
     clashes = 0
     n = len(chromosome)
-
+    # Checking for horizontal and diagonal clashes
     for i in range(n):
         for j in range(i + 1, n):
-            if chromosome[i] == chromosome[j] or abs(chromosome[i] - chromosome[j]) == j - i:
+            if (chromosome[i] == chromosome[j]
+                or abs(chromosome[i] - chromosome[j]) == j - i):
                 clashes += 1
-    
-    # Higher fitness for fewer clashes
-    return 1 / (clashes + 1) 
+    return clashes 
 
 if __name__ == "__main__":
-    board_size = 4
-    curr_population = init_population(board_size, 5)
-    print(curr_population)
-    curr_population[0] = mutate(curr_population[0])
-    print(curr_population)
+    board_size = 8
+    curr_population = Population(
+        board_size, POPULATION_SIZE, calc_fitness, MUTATION_RATE
+    )
+    curr_population.crossover()
+    curr_population.mutate()
+    curr_population.eliminate()
+    print(curr_population[0])
+    if calc_fitness(curr_population[0]) == 0:
+        print("Solution found!") 
---
+++ population.py
@@ -0,0 +1,38 @@
+import random
+
+class Population:
+    def __init__(self, board_size, size, fitness_func, mutation_rate):
+        self.board_size = board_size
+        self.size = size
+        self.mutation_rate = mutation_rate
+        self.fitness_func = fitness_func
+        self.population = [
+        random.sample(range(board_size), board_size) for _ in range(size)
+        ]
+
+    def __getitem__(self, i):
+        return self.population[i]
+    
+    def crossover(self) -> None:
+        for i in range(0, self.size, 2):
+            parent1, parent2 = self.population[i], self.population[i + 1]
+            # Single-point crossove
+            crossover_point = random.randint(0, len(parent1) - 1)
+            child1 = parent1[:crossover_point] + parent2[crossover_point:]
+            child2 = parent2[:crossover_point] + parent1[crossover_point:]
+            self.population.append(child1)
+            self.population.append(child2)
+    
+    def mutate(self) -> None:
+        children = self.population[self.size:]
+        random.shuffle(children)
+        chosen_ones = children[:int(self.size * self.mutation_rate)]
+        for chromosome in chosen_ones:
+            n = len(chromosome)
+            mutation_point = random.randint(0, n - 1)
+            new_gene = random.randint(0, n - 1)
+            chromosome[mutation_point] = new_gene
+
+    def eliminate(self):
+        self.population = sorted(self.population, key=lambda x: self.fitness_func(x))
+        self.population = self.population[:self.size]
