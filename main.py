52f3779d7321a98ee44a8206c71856e39653baf8
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-05-30 11:37:21 +0330

    deletes extra new lines

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from algorithm import genetic_algorithm
 
-
-
 if __name__ == "__main__":
     board_size = int(input("Enter the board size: "))
     best_chromosome, msg, gen = genetic_algorithm(board_size)
-    print(best_chromosome, msg, gen, "steps")
+    print(best_chromosome, msg, gen, )
