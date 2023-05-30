89772b8ccd7d113fffabce85a5ef3a188d787e18
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2023-05-30 11:36:44 +0330

    getting the board size from user

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from algorithm import genetic_algorithm
 
 
 if __name__ == "__main__":
-    board_size = 8
+    board_size = int(input("Enter the board size: "))
     best_chromosome, msg, gen = genetic_algorithm(board_size)
-    print(best_chromosome, msg, gen)
+    print(best_chromosome, msg, gen, "steps")
